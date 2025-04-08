4b2034e477418e28d67646562cf8f6cf9e426a82
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-08 14:42:19 +0200

    Update

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -9,47 +9,47 @@ class BookList extends Component {
     asin: "",
   };
 
-  getReviews = () => {};
-
-  componentDidUpdate = (prevProprs, prevState) => {
-    if (prevProprs.asin !== this.props.asin) {
-      this.getReviews.data;
-    }
+  changeAsin = (asin) => {
+    this.setState({ selectedBook: asin });
   };
 
   render() {
     return (
       <>
-        <Row className='justify-content-center mt-5'>
-          <Col xs={12} md={4} className='text-center'>
-            <Form.Group>
-              <Form.Control
-                type='search'
-                placeholder='Cerca un libro'
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-
-        <Row className='g-2 mt-3'>
-          <Col xs={12} md={9}>
-            <Row>
+        <Row>
+          <Col md={8}>
+            <Row className='justify-content-center mt-5'>
+              <Col xs={12} md={4} className='text-center'>
+                <Form.Group>
+                  <Form.Control
+                    type='search'
+                    placeholder='Cerca un libro'
+                    value={this.state.searchQuery}
+                    onChange={(e) =>
+                      this.setState({ searchQuery: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className='g-2 mt-3'>
               {this.props.books
                 .filter((b) =>
                   b.title.toLowerCase().includes(this.state.searchQuery)
                 )
                 .map((b) => (
                   <Col xs={12} md={4} key={b.asin}>
-                    <SingleBook book={b} />
+                    <SingleBook
+                      book={b}
+                      selectedBook={this.state.selectedBook}
+                      changeSelectedBook={this.changeSelectedBook}
+                    />
                   </Col>
                 ))}
             </Row>
           </Col>
-
-          <Col xs={12} md={3}>
-            <CommentArea />
+          <Col md={4}>
+            <CommentArea asin={this.state.selectedBook} />
           </Col>
         </Row>
       </>
